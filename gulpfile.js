d221fc9b5c7050179e000a4bafd139c87e092a54
Clément <galidie.clement@gmail.com>
2015-06-02 19:06:49 +0200

    add tasks in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,22 @@
-var gulp = require('gulp');
+var gulp = require('gulp'); // Require gulp.
+var plugins = require('gulp-load-plugins'); // Autloading plugins.
+
+// Let's create our styles task.
+gulp.task('styles', function () {
+
+});
+
+// Let's create our scripts task.
+gulp.task('scripts', function () {
+
+});
+
+// Let's create our watch task.
+gulp.task('watch', function () {
+
+});
+
+// Let's create our default task; write 'gulp' in CLI mode to run it.
+gulp.task('default', function () {
+
+});
